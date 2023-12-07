1a345003c300beeb2926d089d4753c006f8d0032
mutasim chaudhary <mutasim.chaud@gmail.com>
2023-12-07 23:01:58 +0000

    comments added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const Shape = require('./lib/shapes.js');
-const Render = require('./lib/render.js');
-const { readFile, writeFile } = require('fs/promises');
+const Render = require('./lib/render.js');//links to classes
+const { readFile, writeFile } = require('fs/promises');//gets read write package
 const questions = [
     {
     type: 'input',
@@ -24,7 +24,7 @@ const questions = [
     name: 'shapeColour',
     message: 'What is your shapes colour?',
     }
-];
+];//question going to be aksed to the user
 
 function init() {
   inquirer.prompt(questions).then((answers) => {
@@ -39,19 +39,19 @@ function init() {
         case 'circle':
           shape = new Shape.Circle();
           break
-      }
+      }//creates shape object from which shape the user chose
       if(answers.text.length > 3){
-        throw new Error('Text is longer than 3 characters');
+        throw new Error('Text is longer than 3 characters');//checks to see if the text for th log is less than 3 if not it throws an error
       }
       const text = new Render.Text(answers.text, answers.textColour)
       shape.setColour(answers.shapeColour);
       const shapeRender = shape.render();
       const textRender = text.render();
       const svg = new Render.SVG();
-      const svgRender = svg.renderSVG(shapeRender, textRender);
-      return writeFile('./dist/logo.svg', svgRender);
+      const svgRender = svg.renderSVG(shapeRender, textRender);//uses all user info to render the info needed to render the svg file
+      return writeFile('./dist/logo.svg', svgRender);//writes file to dist folder
 
-  }).then((res) => console.log("Generated logo.svg"))
+  }).then((res) => console.log("Generated logo.svg"))//after file written prints confirmation message
 }
 
 // Function call to initialize app
